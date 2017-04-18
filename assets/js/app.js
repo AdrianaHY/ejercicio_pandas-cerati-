ecfa9ad3e70a1a6f901632eb8dbdd4755fa38202
AdrianaHY <adriana.herya@gmail.com>
2017-04-18 13:06:02 -0500

    versión cerrando una imagen y apreciendo un párrafo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,26 @@
-var botonUno = document.getElementById("boton1");
-var parrafoUno = document.getElementById("parrafo1");
+var parrafos = document.getElementsByClassName("parrafo")[0];
+var botones= document.getElementsByClassName("boton")[0]
+var arregloBotones=[];
+var arregloParrafos=[];
 var imagen= document.getElementsByClassName("imagen")[0];
 var span = [];
+
+// botonUno.addEventListener("click", mostrarParrafoUno);
+// botonUno.addEventListener("dblclick", ocultarParrafoUno);
 
 for (var i = 0; i < 9; i++){
   span[i]=document.getElementsByClassName("close")[0];
   span[i].addEventListener("click",cerrar);
+}
+
+for (var i = 0; i<2; i++){
+  arregloBotones[i]=document.getElementsByClassName("boton")[0];
+  arregloBotones[i].addEventListener("click",mostrarParrafo);
+}
+
+for (var i = 0; i<2; i++){
+  arregloParrafos[i]=document.getElementsByClassName("parrafo")[0];
+  arregloParrafos[i].addEventListener("dblclick",ocultarParrafo);
 }
 
 function cerrar() {
@@ -13,9 +28,10 @@ function cerrar() {
 }
 
 function ocultarParrafo() {
-  parrafoUno.style.visibility = "hidden";
+  parrafos.style.visibility = "hidden";
 }
 
 function mostrarParrafo(){
-  parrafoUno.style.visibility = "visible";
+  parrafos.style.visibility = "visible";
+
 }
