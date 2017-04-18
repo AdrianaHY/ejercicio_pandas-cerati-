3e03b73ec700b19ca9b858f24e4c5c720c4ccb9f
AdrianaHY <adriana.herya@gmail.com>
2017-04-18 16:29:01 -0500

    Versión final con funcionalidad

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,37 +1,53 @@
-var parrafos = document.getElementsByClassName("parrafo")[0];
-var botones= document.getElementsByClassName("boton")[0]
-var arregloBotones=[];
-var arregloParrafos=[];
-var imagen= document.getElementsByClassName("imagen")[0];
-var span = [];
+var parrafoUno = document.getElementById("parrafo1");
+var botonUno = document.getElementById("boton1")
+var parrafoDos = document.getElementById("parrafo2");
+var botonDos = document.getElementById("boton2")
+var botonRestaurar = document.getElementById("boton3");
+var span = document.getElementsByClassName("close");
 
-// botonUno.addEventListener("click", mostrarParrafoUno);
-// botonUno.addEventListener("dblclick", ocultarParrafoUno);
 
-for (var i = 0; i < 9; i++){
-  span[i]=document.getElementsByClassName("close")[0];
+
+for (var i = 0; i < span.length; i++){
   span[i].addEventListener("click",cerrar);
 }
 
-for (var i = 0; i<2; i++){
-  arregloBotones[i]=document.getElementsByClassName("boton")[0];
-  arregloBotones[i].addEventListener("click",mostrarParrafo);
-}
-
-for (var i = 0; i<2; i++){
-  arregloParrafos[i]=document.getElementsByClassName("parrafo")[0];
-  arregloParrafos[i].addEventListener("dblclick",ocultarParrafo);
-}
 
 function cerrar() {
-  imagen.style.display = "none";
+  var div = this.parentElement;
+  div.style.display = "none";
 }
 
-function ocultarParrafo() {
-  parrafos.style.visibility = "hidden";
+
+botonUno.addEventListener("click",mostrarParrafoUno);
+botonUno.addEventListener("dblclick", ocultarParrafoUno);
+
+function ocultarParrafoUno() {
+  parrafoUno.style.visibility = "hidden";
 }
 
-function mostrarParrafo(){
-  parrafos.style.visibility = "visible";
+function mostrarParrafoUno(){
 
+  parrafoUno.style.visibility = "visible";
+
+}
+
+botonDos.addEventListener("click",mostrarParrafoDos);
+botonDos.addEventListener("dblclick", ocultarParrafoDos);
+
+function ocultarParrafoDos() {
+  parrafoDos.style.visibility = "hidden";
+}
+
+function mostrarParrafoDos(){
+
+  parrafoDos.style.visibility = "visible";
+
+}
+
+botonRestaurar.addEventListener("click", restaurar);
+
+function restaurar(){
+  for(var i=0; i < span.length; i++){
+    span[i].parentElement.style.display= "inline-block";
+  }
 }
